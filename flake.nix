beb23bc61dd8d1f05468748532350a3b2a12b2aa
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2026-04-03 11:41:28 +0100

    project setup

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Impact Sphere Dev Nix Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_24
+            pnpm
+          ];
+
+          shellHook = ''
+            echo "Impact Sphere dev shell"
+            echo "Node $(node --version) | pnpm $(pnpm --version)"
+          '';
+        };
+      });
+}
